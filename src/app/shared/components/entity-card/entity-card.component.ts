f40707120a0d77ca1cf55863b26c22d22d9677d3
Petro Ivankiv <petro.ivankiv88@gmail.com>
2017-06-06 21:15:46 +0300

    Added entity-card component

---
+++ src/app/shared/components/entity-card/entity-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entity-card',
+  templateUrl: './entity-card.component.html',
+  styleUrls: ['./entity-card.component.scss']
+})
+export class EntityCardComponent  {
+}
