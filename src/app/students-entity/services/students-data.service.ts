d1c5b0eea1d455fb168461e66171104fc306a8b1
Petro Ivankiv <petro.ivankiv88@gmail.com>
2017-06-12 20:46:55 +0300

    Changed icon for student

--- src/app/students-entity/services/students-data.service.ts
+++ src/app/students-entity/services/students-data.service.ts
@@ -6,10 +6,10 @@ import {Observable} from 'rxjs/Observable';
 import {Student} from '../models/student-model';
 
 const Students = [
-  new Student('s1', 'John', 'Smith', '1234', 'IT1', 'IT', '../../../assets/01.png'),
-  new Student('s2', 'John', 'Smith', '1234', 'IT2', 'IT', '../../../assets/02.png'),
-  new Student('s3', 'John', 'Smith', '1234', 'IT3', 'IT', '../../../assets/03.png'),
-  new Student('s4', 'John', 'Smith', '1234', 'IT4', 'IT', '../../../assets/04.png')
+  new Student('s1', 'John', 'Smith', '1234', 'IT1', 'IT', '../../../assets/01.jpg'),
+  new Student('s2', 'John', 'Smith', '1234', 'IT2', 'IT', '../../../assets/02.jpg'),
+  new Student('s3', 'John', 'Smith', '1234', 'IT3', 'IT', '../../../assets/03.jpg'),
+  new Student('s4', 'John', 'Smith', '1234', 'IT4', 'IT', '../../../assets/04.jpg')
 ];
 
 @Injectable()
